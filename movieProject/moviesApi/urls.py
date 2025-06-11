6d36b373415a5ad12f36a52296f6af12a33a72a2
abhi-v-10 <v.abhishyanth118@gmail.com>
2025-06-11 11:17:11 +0530

    Deleted moviesApi and changed movieProject.urls

--- movieProject/moviesApi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('post_movie_api/', views.post_movie_api, name="post_movie_api"),
-    path('get_movie_api/', views.get_movie_api, name="get_movie_api"),
-    path('update_movie_api/<int:id>', views.update_movie_api, name="update_movie_api"),
-    path('delete_movie_api/<int:id>', views.delete_movie_api, name="delete_movie_api"),
-]
